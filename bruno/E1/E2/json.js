67a882119078dde7864177ff48fa1cd1bf4bddc3
bruno kreppel <bruno.kreppel@gmail.com>
2023-10-05 11:17:55 +0200

    changed a name

---
+++ bruno/E1/E2/json.js
@@ -0,0 +1,83 @@
+[
+    {
+      "id": "1892471824071",
+      "firstName": "Bruno",
+      "lastName": "Kreppel",
+      "email": "bruno.kreppel@gmail.com",
+      "department": "none",
+      "salary": "1euro"
+    },
+    {
+      "id": "1234567890123",
+      "firstName": "Alice",
+      "lastName": "Johnson",
+      "email": "alice.johnson@email.com",
+      "department": "HR",
+      "salary": "50000"
+    },
+    {
+      "id": "4567890123456",
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "bob.smith@email.com",
+      "department": "IT",
+      "salary": "60000"
+    },
+    {
+      "id": "7890123456789",
+      "firstName": "Eva",
+      "lastName": "Perez",
+      "email": "eva.perez@email.com",
+      "department": "Marketing",
+      "salary": "55000"
+    },
+    {
+      "id": "5678901234567",
+      "firstName": "John",
+      "lastName": "Doe",
+      "email": "john.doe@email.com",
+      "department": "Finance",
+      "salary": "65000"
+    },
+    {
+      "id": "2345678901234",
+      "firstName": "Sarah",
+      "lastName": "Johnson",
+      "email": "sarah.johnson@email.com",
+      "department": "Sales",
+      "salary": "60000"
+    },
+    {
+      "id": "8901234567890",
+      "firstName": "Peter",
+      "lastName": "Brown",
+      "email": "peter.brown@email.com",
+      "department": "Operations",
+      "salary": "55000"
+    },
+    {
+      "id": "3456789012345",
+      "firstName": "Emily",
+      "lastName": "Davis",
+      "email": "emily.davis@email.com",
+      "department": "Legal",
+      "salary": "70000"
+    },
+    {
+      "id": "9012345678901",
+      "firstName": "Michael",
+      "lastName": "Smithson",
+      "email": "michael.smithson@email.com",
+      "department": "Engineering",
+      "salary": "75000"
+    },
+    {
+      "id": "6789012345678",
+      "firstName": "Grace",
+      "lastName": "Lee",
+      "email": "grace.lee@email.com",
+      "department": "Research",
+      "salary": "62000"
+    }
+  ]
+  
